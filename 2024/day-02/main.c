b547540d04585e9e0db94f0c92f5fb5c63b0dd60
Gabriel Toll Stålbom <gabtost@gmail.com>
2024-12-03 19:30:43 +0100

    solve 2024 day 2 part 2

--- 2024/day-02/main.c
+++ 2024/day-02/main.c
@@ -2,50 +2,95 @@
 #include <stdlib.h>
 #include <string.h>
 
-int solution_part_1() {
-    char buff[256];
-    int answer = 0;
+void copy_arr_except_idx(int target[], int source[], int skip_idx) {
+    int i = 0;
+    int j = 0;
 
-    FILE *file = fopen("input", "r");
-
-    while (fgets(buff, sizeof(buff), file)) {
-        int curr = atoi(strtok(buff, " "));
-        char* next = strtok(NULL, " ");
-        int dir = 0;
-        int safe = 1;
-
-        while (next != NULL) {
-            int n = atoi(next);
-            int diff = abs(curr - n);
-
-            if (diff < 1 || diff > 3) {
-                safe = 0;
-                break;
-            }
-
-            if (dir == 0) {
-                dir = n > curr ? 1 : -1;
-            } else if ((dir == 1 && n < curr) || (dir == -1 && n > curr) ) {
-                safe = 0;
-                break;
-            }
-
-            curr = n;
-            next = strtok(NULL, " ");
+    while (source[i] != '\0') {
+        if (i == skip_idx) {
+            i++;
+            continue;
         }
 
-        if (safe) {
-            answer++;
+        target[j++] = source[i++];
+    }
+}
+
+void str_to_int_arr(int target[], char* source, char *delim) {
+    char* token = strtok(source, delim);
+    int i = 0;
+
+    while (token != NULL) {
+        target[i++] = atoi(token);
+        token = strtok(NULL, delim);
+    }
+}
+
+int is_safe(int arr[]) {
+    int asc = arr[0] < arr[1];
+
+    for (int i = 0; ; i++) {
+        if (arr[i + 1] == '\0') {
+            break;
+        }
+
+        int curr = arr[i];
+        int next = arr[i + 1];
+        int diff = abs(curr - next);
+
+        // ensure diff is between 1 and 3 and that the numbers are in order
+        if (
+            (diff < 1 || diff > 3) ||
+            (asc == 1 && curr > next) ||
+            (asc == 0 && curr < next)
+        ) {
+            return 0;
         }
     }
 
-    return answer;
+    return 1;
 }
 
 int main() {
-    int part1 = solution_part_1();
+    int part1 = 0;
+    int part2 = 0;
+    char buff[32];
+
+    FILE *file = fopen("input", "r");
+
+    // loop through each line in the file
+    while (fgets(buff, sizeof(buff), file)) {
+        // convert the line to an array of numbers
+        int arr[9] = {};
+        str_to_int_arr(arr, buff, " ");
+
+        // check if the entire array is safe (part 1)
+        if (is_safe(arr)) {
+            part1++;
+            part2++;
+            continue;
+        }
+
+        // check if any one number can be removed to make the line safe (part 2)
+        for (int j = 0; ; j++) {
+            if (arr[j] == '\0') {
+                break;
+            }
+
+            int arr_cpy[9] = {};
+            copy_arr_except_idx(arr_cpy, arr, j);
+
+            if (is_safe(arr_cpy)) {
+                part2++;
+                break;
+            }
+        }
+    }
+
+    fclose(file);
 
     printf("part 1: %i\n", part1);
+    printf("part 2: %i\n", part2);
 
     return 0;
 }
